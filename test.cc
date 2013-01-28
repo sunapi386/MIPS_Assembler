eeb1549a51f86f872ca0e0708c5e314606f12170
sunapi386 <sunapi386@gmail.com>
2013-01-28 12:04:21 -0500

    Updated github.sh with messages

--- test.cc
+++ test.cc
@@ -1,38 +1,21 @@
 // Test
 #include <iostream>
-#include <cstdlib>
-#include <sstream>
-#include <string>
+#include <bitset>
+#include <climits>
+
 using namespace std;
 
-string decimalStringToBinaryString (string decimalString) {
-   string BinaryString = "";
-   int decimalNumber = atoi (decimalString.c_str());
-   if (decimalNumber < 0) { 
-      cerr << "ERROR decimalStringToBinaryString: negative value" << endl;
-      exit(1);
+template<typename T>
+void show_binrep (const T& a) {
+   const char* beg = reinterpret_cast<const char*> (&a);
+   const char* end = beg + sizeof (a);
+   while (beg != end) {
+      cout << bitset<32>(*beg++) << ' ';
    }
-   while (decimalNumber != 0) {
-      int remainder = decimalNumber % 2;
-      string r;
-      ostringstream convert;
-      convert << remainder;
-      r = convert.str();
-      BinaryString.insert (0, r);
-      decimalNumber /= 2;
-   }
-   string zero = "0";
-   while (BinaryString.length() < 32) {
-      BinaryString.insert (0, zero);
-   }
-   return BinaryString;
+   cout << '\n';
 }
 
-
-
 int main() {
-    string decimalString;
-    cout << "enter in a decimal number to be converted to binary\n";
-    cin >> decimalString;
-    cout << decimalStringToBinaryString (decimalString) << endl;
+   int n = 30;
+   show_binrep(n);
 }
